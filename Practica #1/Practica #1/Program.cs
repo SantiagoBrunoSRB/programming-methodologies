c866f7409ae5d9e48d83da678de4f82b7ba245a9
SantiagoBrunoSRB <santiagorafaelbrunosrb@gmail.com>
2026-04-29 02:50:27 -0300

    Ejercicio 5 : Implementacion de la funcion llenar en el Main

--- Practica #1/Practica #1/Program.cs
+++ Practica #1/Practica #1/Program.cs
@@ -7,6 +7,8 @@
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
 using System;
+using System.Collections;
+
 
 namespace Practica__1
 {
@@ -17,9 +19,42 @@ namespace Practica__1
         static void Main(string[] args)
         {
         	
-        	Console.WriteLine("Hola ");
-        	Console.ReadKey();
+        	
+			
+        		Pila p= new Pila();
+			Numero num1 = new Numero(29);
+			Numero num2 = new Numero(22);
+			Numero num3 = new Numero(23);
+			Numero num4 = new Numero(242);
+			p.apilar(num1);
+			p.apilar(num2);
+			p.apilar(num3);
+			p.apilar(num4);
+			Console.WriteLine(p.getPila());
+			Console.WriteLine(p.desapilar());
+			Console.WriteLine(p.maximo());
+			Console.WriteLine(p.minimo());
+        	Console.WriteLine(p.contiene(new Numero(24)));
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+
+		// metodos de la clase 
+		public static void llenar(Icoleccionable c){
+			Random azar = new Random();
+			for (int i = 0; i < 20; i++) {
+				int valor = azar.Next(1,100);
+				Comparable com = new Numero(valor);
+				c.agregar(com);
+			}
+		}
+        	
+        	
+        	
+        	
+        	
+        	
            
-    }
+    
 }
 }
