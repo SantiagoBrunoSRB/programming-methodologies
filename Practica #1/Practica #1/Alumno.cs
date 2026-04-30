31079a7ea35b6b96b64c19822e6188f5599dc480
SantiagoBrunoSRB <santiagorafaelbrunosrb@gmail.com>
2026-04-30 02:22:35 -0300

    Ejercicio 12: Implentacion de la Clase Alumno y Respuesta a Ejercicio 10

---
+++ Practica #1/Practica #1/Alumno.cs
@@ -0,0 +1,40 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: Santiago
+ * Fecha: 30/04/2026
+ * Hora: 2:08
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+
+namespace Practica__1
+{
+	/// <summary>
+	/// Description of Alumno.
+	/// </summary>
+	public class Alumno : Persona
+	{
+		//Propiedades
+		private int legajo;
+		private double promedio;
+		
+		//Constructor
+		public Alumno(string nom, int doc, int l, double p) 
+			:base(nom,doc)
+		{
+			legajo= l;
+			promedio= p;
+			
+		}
+		//getters
+		public int getLegajo(){
+			return this.legajo;
+		}
+		
+		public double getPromedio(){
+			return this.promedio;
+		}
+		
+	}
+}
