74f32f4f3cae8e68f7bfeb0cd945ca7d0382780f
SantiagoBrunoSRB <santiagorafaelbrunosrb@gmail.com>
2026-04-29 23:15:14 -0300

    Ejercicio 6: Implementacion de la funcion informar

--- Practica #1/Practica #1/Program.cs
+++ Practica #1/Practica #1/Program.cs
@@ -7,7 +7,7 @@
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
 using System;
-using System.Collections;
+
 
 
 namespace Practica__1
@@ -15,46 +15,47 @@ namespace Practica__1
 	/// <summary>
 	/// Description of Program.
 	/// </summary>
-	class Program{
-        static void Main(string[] args)
+	class Program
+	{
+        public static void Main(string[] args)
         {
-        	
-        	
-			
-        		Pila p= new Pila();
-			Numero num1 = new Numero(29);
-			Numero num2 = new Numero(22);
-			Numero num3 = new Numero(23);
-			Numero num4 = new Numero(242);
-			p.apilar(num1);
-			p.apilar(num2);
-			p.apilar(num3);
-			p.apilar(num4);
-			Console.WriteLine(p.getPila());
-			Console.WriteLine(p.desapilar());
-			Console.WriteLine(p.maximo());
-			Console.WriteLine(p.minimo());
-        	Console.WriteLine(p.contiene(new Numero(24)));
-			Console.Write("Press any key to continue . . . ");
-			Console.ReadKey(true);
+        	Console.WriteLine("Las interfaces estan vivas!!");
+        	Console.ReadKey(true);
 		}
 
-		// metodos de la clase 
-		public static void llenar(Icoleccionable c){
+		// metodos de la clase
+		public static void llenar(Icoleccionable c)
+		{
 			Random azar = new Random();
-			for (int i = 0; i < 20; i++) {
-				int valor = azar.Next(1,100);
+			for (int i = 0; i < 20; i++)
+			{
+				int valor = azar.Next(1, 100);
 				Comparable com = new Numero(valor);
 				c.agregar(com);
 			}
 		}
-        	
-        	
-        	
-        	
-        	
-        	
-           
-    
-}
+
+		public static void informar(Icoleccionable c)
+		{
+			//cuantos
+			Console.WriteLine("tamaño del Coleccionable: " + c.cuantos());
+			//minimo
+			Console.WriteLine("minimo: " + c.minimo());
+			//maximo
+			Console.WriteLine("maximo: " + c.maximo());
+			//contiene
+			Console.WriteLine("Ingrese un valor para buscar en la Coleccion: ");
+			int read = int.Parse(Console.ReadLine());
+			Comparable com = new Numero(read);
+			if (c.contiene(com))
+			{
+				Console.WriteLine("el elemento se encuentra en la lista");
+			}
+			else
+			{
+				Console.WriteLine("el elemento no se encuentra en la lista");
+			}
+		}
+
+	}
 }
