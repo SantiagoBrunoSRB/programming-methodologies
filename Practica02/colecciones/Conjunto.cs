604f3ce9715fd0eb22fe4595b86674df02fcaed5
SantiagoBrunoSRB <santiagorafaelbrunosrb@gmail.com>
2026-05-08 02:10:10 -0300

    Ejercicio 4: Implementacion de la interfaz Icoleccionable en Conjunto.cs

--- Practica02/colecciones/Conjunto.cs
+++ Practica02/colecciones/Conjunto.cs
@@ -6,19 +6,20 @@ namespace Practica02
 	/// <summary>
 	/// Description of Conjunto.
 	/// </summary>
-	public class Conjunto
+	public class Conjunto : IColeccionable
 	{
 		private List<Comparable> listaElementos;
 		public Conjunto()
 		{
 			listaElementos = new List<Comparable>();
 		}
-		
+
 		public void agregar(Comparable c){
 			if(!pertenece(c)){
 				this.listaElementos.Add(c);
 			}
 		}
+		
 		public bool pertenece(Comparable c){
 			foreach (Comparable e in this.listaElementos) {
 				if(c.sosIgual(e)){
@@ -26,6 +27,56 @@ namespace Practica02
 				}
 			}
 			return false;
+		}
+		
+		//getter del List<Comparable>
+		
+		public List<Comparable> getListaElementos(){
+			return this.listaElementos;
+		}
+		// metodos  de la implementacion de la  INTERFAZ IColeccionable.cs
+		public int cuantos()
+		{
+			return this.listaElementos.Count;
+		}
+
+		public Comparable maximo()
+		{
+			//si la lista posee elementos
+			if(this.listaElementos.Count > 0 ){
+				Comparable valorInicial = this.listaElementos[0];
+				foreach (Comparable e in this.listaElementos) {
+					if(!valorInicial.sosMayor(e)){
+						valorInicial = e;
+					}
+				}
+				return valorInicial;
+				
+			}else
+				throw new Exception("La lista esta vacia");
+			//caso contratio mandamos un mensajito
+		}
+
+		public Comparable minimo()
+		{
+			//si la lista posee elementos
+			if(this.listaElementos.Count > 0 ){
+				Comparable valorInicial = this.listaElementos[0];
+				foreach (Comparable e in this.listaElementos) {
+					if(!valorInicial.sosMenor(e)){
+						valorInicial = e;
+					}
+				}
+				return valorInicial;
+				
+			}else
+				throw new Exception("la lista esta vacia");
+			//caso contratio mandamos un mensaje
+		}
+
+		public bool contiene(Comparable c)
+		{
+			return pertenece(c);
 		}
 	}
 }
