c9a889cb9a809cc5b00ceda60ec7b146111e6f1e
SantiagoBrunoSRB <santiagorafaelbrunosrb@gmail.com>
2026-05-11 02:33:35 -0300

    Ejercicio 8: Creacion clase Profesor

---
+++ Practica03/Profesor.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: Santiago
+ * Fecha: 11/05/2026
+ * Hora: 2:32
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+
+namespace Practica03
+{
+	/// <summary>
+	/// Description of Profesor.
+	/// </summary>
+	public class Profesor : Persona
+	{
+		//atributos
+		private int antiguedad;
+		//constructor
+		public Profesor(string nom,int doc,int ant):base(nom,doc)
+		{
+			this.antiguedad = ant;
+		}
+		//getter
+		public int getAntiguedad(){
+			return this.antiguedad;
+		}
+		public void hablarALaClase(){
+			Console.WriteLine("Hablando de algun tema");
+		}
+		public void escribirEnElPizarron(){
+			Console.WriteLine("Escribiendo en el pizarron");
+		}
+	}
+}
