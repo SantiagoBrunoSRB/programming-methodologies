6588dfa14517540b83af7b4c33268386df5ad249
SantiagoBrunoSRB <santiagorafaelbrunosrb@gmail.com>
2026-05-26 01:31:27 -0300

    Ejercicio 4: Implementacion de una funcion main que instancia Teacher

--- Practica04/Program.cs
+++ Practica04/Program.cs
@@ -4,7 +4,7 @@
  * Fecha: 28/3/2026
  * Hora: 19:14
  * 
- * Para cambiar esta plantilla use Herramientas | Opciones | Codificaci�n | Editar Encabezados Est�ndar
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
  */
 using System;
 using System.Collections.Generic;
@@ -12,6 +12,8 @@ using Practica04._iterator;
 using Practica04._strategy;
 using Practica04._factory_method;
 using Practica04._observer;
+using Practica04._adapter;
+using MetodologíasDeProgramaciónI;
 
 
 namespace Practica04
@@ -25,27 +27,19 @@ namespace Practica04
 		{
 			//================ MAIN Consola===============================================
 
+			Teacher teacher = new Teacher();
 			
-			
-				//ejercicio 7 TP3
-			/*IColeccionable coleccion = FabricaDeColeccionables.crearColeccion(4);
-			llenar(coleccion,3);
-			informar(coleccion,3);
-			*/
-			
-			Profesor profe = new Profesor("Mauro",1234567,7);
-			Pila p = new Pila();
-			llenar(p,2);
-			Iterador ite = p.crearIterador(); // Iterador de pila
-			//definimos el primer elemento del iterador 
-			ite.primero();
-			// se ejecuta mientras el iterador no llego al final de la lista
-			while (!ite.fin()) {
-				profe.agregarObservador((Observador)ite.actual()); //ite.actual() --> me tira  un Comparable
-				ite.siguente();
+			for (int i = 0; i < 10; i++) {
+				Alumno alumno = new Alumno("Alumno " + (i + 1), 1000 + i, i + 1, 7);
+				teacher.goToClass(new AdaptadorDeAlumno(alumno));
 			}
-			dictadoDeClases(profe);
 			
+			for (int i = 0; i < 10; i++) {
+				AlumnoMuyEstudioso alumno = new AlumnoMuyEstudioso("Alumno muy estudioso " + (i + 1), 2000 + i, i + 11, 10);
+				teacher.goToClass(new AdaptadorDeAlumno(alumno));
+			}
+			
+			teacher.teachingAClass();
 			
 			Console.WriteLine("Hello estan vivas");
 			Console.Write("Press any key to continue . . . ");
@@ -71,7 +65,7 @@ namespace Practica04
 		//Tp3 Modificamos  informar  ejercicio 6
 		public static void informar(IColeccionable c,int opcion){
 			// Imprime cuantos elementos contiene el coleccionable
-			Console.WriteLine("tama�o del Coleccionable: "+c.cuantos());
+			Console.WriteLine("tamaño del Coleccionable: "+c.cuantos());
 			//Imprime el minimo del coleccionable
 			Console.WriteLine("minimo: "+c.minimo());
 			//Imprime el maximo del coleccionable
