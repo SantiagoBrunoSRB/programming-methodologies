f5e0f423dfa39f52f5af595ff929c85979ffabd5
SantiagoBrunoSRB <santiagorafaelbrunosrb@gmail.com>
2026-06-05 01:31:26 -0300

    Implementacion de la interfaz OrdenEnAula2

---
+++ Practica05/OrdenEnAula2.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Practica05
+{
+	/// <summary>
+	/// Description of OrdenEnAula2.
+	/// </summary>
+	public interface OrdenEnAula2
+	{
+		void ejecutar(Comparable c);
+	}
+}
